895889f632ab9775121d88211e3e053d0a64d0f8
Gabriell Tavares <gabrielltavsb@gmail.com>
2021-07-19 12:52:39 -0300

    refact sin_an to class

--- Trab2/sin_an.cpp
+++ Trab2/sin_an.cpp
@@ -18,117 +18,140 @@ Helbert Moreira Pinto - 10716504
 using namespace std;
 
 #define ERRO 0;
-
-bool obter_simbolo(string token)
+class sin_an
 {
-    cout << token << endl;
-    return true;
-}
+public:
+    vector<string> tokens;
 
-int numero(vector<string> tokens, int i)
-{
-    obter_simbolo(tokens[i++]);
-    return i;
-}
+    sin_an(vector<string> tkns) : tokens(tkns) {}
 
-int dc_c(vector<string> tokens, int i)
-{
-    if (tokens[i] == "simb_const")
-        obter_simbolo(tokens[i]);
-    else
-        cout << "Erro sintatico!" << endl;
-    i++;
-    if (tokens[i] == "simb_ident")
-        obter_simbolo(tokens[i]);
-    else
-        cout << "Erro sintatico!" << endl;
-    i++;
-    if (tokens[i] == "simb_igual")
-        obter_simbolo(tokens[i]);
-    else
-        cout << "Erro sintatico!" << endl;
-    i++;
-    i = numero(tokens, i);
-    if (tokens[i] == "simb_dp")
-        obter_simbolo(tokens[i]);
-    else
-        cout << "Erro sintatico!" << endl;
-    i++;
-    return i;
-}
+    bool obter_simbolo(string token)
+    {
+        cout << token << endl;
+        return true;
+    }
 
-int dc_v(vector<string> tokens, int i)
-{
-    obter_simbolo(tokens[i++]);
-    return i;
-}
+    int numero(vector<string> tokens, int i)
+    {
+        obter_simbolo(tokens[i++]);
+        return i;
+    }
 
-int dc_p(vector<string> tokens, int i)
-{
-    obter_simbolo(tokens[i++]);
-    return i;
-}
+    int dc_c(vector<string> tokens, int i)
+    {
+        if (tokens[i] == "simb_const")
+            obter_simbolo(tokens[i]);
+        else
+            cout << "Erro sintatico!" << endl;
+        i++;
+        if (tokens[i] == "simb_ident")
+            obter_simbolo(tokens[i]);
+        else
+            cout << "Erro sintatico!" << endl;
+        i++;
+        if (tokens[i] == "simb_igual")
+            obter_simbolo(tokens[i]);
+        else
+            cout << "Erro sintatico!" << endl;
+        i++;
+        i = numero(tokens, i);
+        if (tokens[i] == "simb_dp")
+            obter_simbolo(tokens[i]);
+        else
+            cout << "Erro sintatico!" << endl;
+        i++;
+        return i;
+    }
 
-int dc(vector<string> tokens, int i)
-{
-    i = dc_c(tokens, i);
-    i = dc_v(tokens, i);
-    i = dc_p(tokens, i);
-    return i;
-}
+    int dc_v(vector<string> tokens, int i)
+    {
+        obter_simbolo(tokens[i++]);
+        return i;
+    }
 
-int comandos(vector<string> tokens, int i)
-{
-    obter_simbolo(tokens[i++]);
-    return i;
-}
+    int dc_p(vector<string> tokens, int i)
+    {
+        obter_simbolo(tokens[i++]);
+        return i;
+    }
 
-int corpo(vector<string> tokens, int i)
-{
-    i = dc(tokens, i);
-    if (tokens[i] == "simb_begin")
-        obter_simbolo(tokens[i]);
-    else
-        cout << "Erro sintatico!" << endl;
-    i++;
-    i = comandos(tokens, i);
-    if (tokens[i] == "simb_end")
-        obter_simbolo(tokens[i]);
-    else
-        cout << "Erro sintatico!" << endl;
-    i++;
-    return i;
-}
+    int dc(vector<string> tokens, int i)
+    {
+        i = dc_c(tokens, i);
+        i = dc_v(tokens, i);
+        i = dc_p(tokens, i);
+        return i;
+    }
 
-bool programa(vector<string> tokens, int i)
-{
-    if (tokens[i] == "simb_program")
-        obter_simbolo(tokens[i]);
-    else
-        cout << "Erro sintatico!" << endl;
-    i++;
-    if (tokens[i] == "simb_ident")
-        obter_simbolo(tokens[i]);
-    else
-        cout << "Erro sintatico!" << endl;
-    i++;
-    if (tokens[i] == "simb_pv")
-        obter_simbolo(tokens[i]);
-    else
-        cout << "Erro sintatico!" << endl;
-    i++;
-    i = corpo(tokens, i);
-    if (tokens[i] == "simb_dot")
-        obter_simbolo(tokens[i]);
-    else
-        cout << "Erro sintatico!" << endl;
-    i++;
-    return true;
-}
+    int comandos(vector<string> tokens, int i)
+    {
+        obter_simbolo(tokens[i++]);
+        return i;
+    }
+
+    int corpo(vector<string> tokens, int i)
+    {
+        i = dc(tokens, i);
+        if (tokens[i] == "simb_begin")
+            obter_simbolo(tokens[i]);
+        else
+            cout << "Erro sintatico!" << endl;
+        i++;
+        i = comandos(tokens, i);
+        if (tokens[i] == "simb_end")
+            obter_simbolo(tokens[i]);
+        else
+            cout << "Erro sintatico!" << endl;
+        i++;
+        return i;
+    }
+
+    bool programa(vector<string> tokens, int i)
+    {
+        if (tokens[i] == "simb_program")
+            obter_simbolo(tokens[i]);
+        else
+            cout << "Erro sintatico!" << endl;
+        i++;
+        if (tokens[i] == "simb_ident")
+            obter_simbolo(tokens[i]);
+        else
+            cout << "Erro sintatico!" << endl;
+        i++;
+        if (tokens[i] == "simb_pv")
+            obter_simbolo(tokens[i]);
+        else
+            cout << "Erro sintatico!" << endl;
+        i++;
+        i = corpo(tokens, i);
+        if (tokens[i] == "simb_dot")
+            obter_simbolo(tokens[i]);
+        else
+            cout << "Erro sintatico!" << endl;
+        i++;
+        return true;
+    }
+
+    void processaTokens()
+    {
+        programa(tokens, 0);
+    }
+
+    void imprimeTokens()
+    {
+        cout << "Quantidade de tokens: " << tokens.size() << endl;
+        for (int i = 0; i < tokens.size(); i++)
+        {
+            cout << tokens[i] << endl;
+        }
+    }
+};
 
 int main()
 {
     vector<string> tokens{"simb_program", "simb_ident", "simb_pv", "dc", "simb_begin", "comandos", "simb_end", "simb_dot"};
-    programa(tokens, 0);
+    sin_an SinAn(tokens);
+    SinAn.imprimeTokens();
+    SinAn.processaTokens();
     return 0;
 }
